867f4b1432d7c61e8bd9a22742ca6329845c69ef
eoleedi <eoleedimin@gmail.com>
2020-03-18 15:41:42 +0800

    admin func

--- app.py
+++ app.py
@@ -37,11 +37,13 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     if(event.source.user_id == 'Uc38ec3a6672b3b5033dddc4851ad4893'): 
+        login = TextSendMessage(text="login success")
+        line_bot_api.reply_message(event.reply_token, login)
         for userid in user_id.keys():
             line_bot_api.push_message(userid, message)
-
-    line_bot_api.reply_message(event.reply_token, message)
-    user_id[event.source.user_id]: "RUNOOB"
+    else:
+        line_bot_api.reply_message(event.reply_token, message)
+        user_id[event.source.user_id]: "RUNOOB"
 '''
 #admin
 app2 = Flask(__name__)
