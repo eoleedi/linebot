e549866a8dbf52c859a6b208e6c710ac8d867a82
eoleedi <eoleedimin@gmail.com>
2020-03-18 13:39:24 +0800

    Add code

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, message + "2323")
     message2 = TextSendMessage(text='No')
     line_bot_api.push_message(event.push_token, message2)
 
