a1a86b9094d44e7b6b825ed8b10a696d3ec7d878
eoleedi <eoleedimin@gmail.com>
2020-03-25 22:21:28 +0800

    update

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ line_bot_api = LineBotApi('D4jP2o+UJxNhEPro+12EqFl7HUa8iHyfabFIxtTXjYx/tLm2QAEDJ
 # Channel Secret
 handler = WebhookHandler('91d2018622705a3117d64eb67044f573')
 
-header = {
-    "Authorization: Bearer D4jP2o+UJxNhEPro+12EqFl7HUa8iHyfabFIxtTXjYx/tLm2QAEDJqY2f6KmrqfDepOhTigfWzCJS2ttTjQXSNcA0RHsLqS+6d2W3/LSzWxYbRaAyIhrsnRxxRNuAxXaUiOg6rkqUpwSCEmtqFL6+QdB04t89/1O/w1cDnyilFU="
-}
 def getDisplayName(userid):
+    header = {
+    "Authorization: Bearer D4jP2o+UJxNhEPro+12EqFl7HUa8iHyfabFIxtTXjYx/tLm2QAEDJqY2f6KmrqfDepOhTigfWzCJS2ttTjQXSNcA0RHsLqS+6d2W3/LSzWxYbRaAyIhrsnRxxRNuAxXaUiOg6rkqUpwSCEmtqFL6+QdB04t89/1O/w1cDnyilFU="
+    }
     response = requests.get('https://api.line.me/v2/bot/profile/'+ userid, headers= header)
     return response
 # 監聽所有來自 /callback 的 Post Request
@@ -47,15 +47,16 @@ def callback():
 def handle_message(event):
     displayName = getDisplayName(event.source.user_id)
     
-    
+    #connect to database
     DATABASE_URL = os.environ['DATABASE_URL']
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
+
     #第一次加入，儲存userid
-    
     if (cursor.execute("SELECT COUNT(*) from users where userid = %s", [event.source.user_id]) != 0):
         cursor.execute("INSERT INTO users(userID,status,displayName) VALUES(%s,%s)",[event.source.user_id, '',displayName])
 
+    #get user status
     cursor.execute("SELECT status from users where userID = %s", [event.source.user_id])
     status = cursor.fetchone()
 
@@ -64,26 +65,83 @@ def handle_message(event):
     if(status == "Addroomid"):
         #離開管理者模式
         if(message == "break"):
-            cursor.execute("UPDATE USERS SET status = %s where userid = %s ",['',event.source.user_id])
+            cursor.execute("UPDATE USERS SET status = %s WHERE userid = %s",['',event.source.user_id])
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "離開加入管理者模式"))
             conn.commit()
             conn.close()
             return 0
         #檢查 roomid 存在
-        if (cursor.execute("SELECT COUNT(*) from rooms where roomid = %s", [message]) == 0):
+        ##########
+        #不存在
+        if (cursor.execute("SELECT COUNT(*) from rooms where roomID = %s", [message]) == 0): 
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "addroom failed"))
             conn.commit()
             conn.close()
             return 0
+        #存在
         else:    
             cursor.execute("INSERT INTO admin(adminID,roomID) VALUES(%s,%s)", [event.source.user_id, message])
-            print('admin add success')
+            cursor.execute("UPDATE rooms SET adminID = %s WHERE roomID = %s",[event.source.user_id, message])
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "addroom success"))
+
+   
+    elif(status == "Gaming"):
 
     if(message.find("管理者") != -1):
         cursor.execute("UPDATE USERS SET status = %s WHERE userid = %s ",['Addroomid', event.source.user_id])
         roomIdRequest = TextSendMessage(text = "請輸入roomid")
         line_bot_api.reply_message(event.reply_token, roomIdRequest)
+    
 
+
+
+    conn.commit()
+    conn.close()
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    postback = event.postback.data
+    
+    #connect to database
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor()
+
+    if(postback == "start" ):
+        cursor.execute("INSERT INTO rooms(roomID) VALUES(%s)", [event.source.room])
+        roomIdSent = TextSendMessage(text = event.source.room)
+        line_bot_api.reply_message(event.reply_token, roomIdSent)
+    
+    cursor.commit()
+    cursor.close()
+
+
+@handler.add(JoinEvent)
+def handle_join(event):
+    introduction = "我是你們的資工小精靈，接下來的越獄之路，我會在這裡陪伴你們的"
+    buttons_template = TemplateSendMessage(
+        alt_text=introduction,
+        template = ButtonsTemplate(
+            title = introduction,
+            text = "",
+            actions=[
+                PostbackTemplateAction(
+                    label = "start",
+                    text = "start",
+                    data = "start"
+                )
+            ]
+        )
+    )
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+
+#note
+#string.find()尋找關鍵字
+'''
     if(event.source.user_id == 'Uc38ec3a6672b3b5033dddc4851ad4893'): 
         if(message == 'login'):
             #login
@@ -95,29 +153,4 @@ def handle_message(event):
                 user = TextSendMessage(text = userid)
                 line_bot_api.reply_message(event.source.user_id, user)
             f_userid.close()
-    else:
-        line_bot_api.reply_message(event.reply_token, message)
-        fo = open('user_id.txt', 'r')
-        if event.source.user_id not in fo.read():
-            f = open('user_id.txt', 'w+')
-            f.write(event.source.user_id + '\n')
-        f.close()
-        fo.close()
-
-    conn.commit()
-    conn.close()
-
-@handler.add(PostbackEvent)
-def handle_postback(event):
-    postback = event.postback.data
-
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
-
-
-#note
-#string.find()尋找關鍵字
+    '''
